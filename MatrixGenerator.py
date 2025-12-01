8724953cf5693c152b8d1229fd5449a2bd0d78fa
Aleksei Kurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2025-12-01 12:57:17 +0300

    microfix

--- MatrixGenerator.py
+++ MatrixGenerator.py
@@ -11,6 +11,7 @@ class MatrixGenerator:
         self.a_max = a_max
         self.beta1 = beta1
         self.beta2 = beta2
+        
     @private
     def _validate_parameters(self, n: int, v: int, a_min: float, a_max: float, beta1: float, beta2: float):
         if n <= 0:
@@ -24,20 +25,21 @@ class MatrixGenerator:
         if not (0.85 <= beta1 < beta2 <= 1.0):
                 raise ValueError("beta1 and beta2 must be in range [0.85, 1.0]")
                 
-    def Generate_C_Matrix(self, distribution_type: str = "uniform") -> np.array:
-    a_vector, b_matrix = self.Generate_ab_Matrices(distribution_type)
+    def GenerateCMatrix(self, distribution_type: str = "uniform") -> np.array:
+        a_vector, b_matrix = self.Generate_ab_Matrices(distribution_type)
+        
+        n, v = b_matrix.shape
+        c_matrix = np.zeros((n, v))
+        
+        c_matrix[:, 0] = a_vector # c_i1 = a_i
+        
+        for j in range(1, v):
+            c_matrix[:, j] = c_matrix[:, j-1] * b_matrix[:, j-1]
+        
+        return c_matrix
     
-    n, v = b_matrix.shape
-    c_matrix = np.zeros((n, v))
-    
-    c_matrix[:, 0] = a_vector # c_i1 = a_i
-    
-    for j in range(1, v):
-        c_matrix[:, j] = c_matrix[:, j-1] * b_matrix[:, j-1]
-    
-    return c_matrix
     @private
-    def Generate_ab_Matrices(self, distribution_type: str) -> Tuple[np.array, np.array]:
+    def GenerateABMatrices(self, distribution_type: str) -> Tuple[np.array, np.array]:
         a_vector = np.array([np.random.uniform(self.a_min, self.a_max) for _ in range(self.n)])  # вектор начальной сахаристости
 
         if distribution_type == "uniform":
@@ -55,6 +57,6 @@ class MatrixGenerator:
                 for j in range(self.v):
                     b_matrix[i][j] = np.random.uniform(beta1_i, beta2_i)
         else:
-            raise ValueError("Distribution_type must be 'uniform' or 'concentrated'")
+            raise ValueError("Distribution type must be 'uniform' or 'concentrated'")
         
         return a_vector, b_matrix
